e4eb6c4dbe580391927fc51a675418467af6bc15
sammycliffy <sammycliffy15@gmail.com>
2020-06-17 15:40:45 +0100

    init

--- lib/intro_pages/second_intro.dart
+++ lib/intro_pages/second_intro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stockfare_mobile/intro_pages/third_intro.dart';
 
 class SecondIntro extends StatelessWidget {
   @override
@@ -37,13 +38,19 @@ class SecondIntro extends StatelessWidget {
               SizedBox(
                 height: 80,
               ),
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey),
-                child: Icon(Icons.add, color: Colors.white, size: 40),
+              GestureDetector(
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey),
+                  child: Icon(Icons.add, color: Colors.white, size: 40),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ThirdIntro()));
+                },
               ),
               SizedBox(
                 height: 10,
